bb87890b756828e6082abc731578eb543c01ba39
styndi <boudewijnstuijk.bs91@gmail.com>
2022-05-30 16:38:45 +0200

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+
+export const App = () => {
+    return (<>Hello world </>)
+
+}
+
+/*1. render all meme templates
+* 2. create meme page, imputs and buttons
+* 3. generate url to get meme
+* 4. display meme from response*/
