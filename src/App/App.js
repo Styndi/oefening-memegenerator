7ae24d4caa3d3abe409d20eef7010790d05c9d2c
styndi <boudewijnstuijk.bs91@gmail.com>
2022-05-30 16:43:31 +0200

    Generate/skip werken. username password zijn incorrect

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
-
 import React from "react";
+import {Meme} from "../Meme/Meme";
+/*import styles from './styles.module.css'*/
 
 
 export const App = () => {
-    return (<>Hello world </>)
+    return (<Meme />)
 
 }
 
